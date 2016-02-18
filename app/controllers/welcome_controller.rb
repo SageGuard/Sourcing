2054736ee314978c648807fde8c2484a83fab7e1
Anthony Mortimer <amortimer@sageguard.com>
2016-02-17 20:33:55 -0500

    Created project seeds, randomized projects on homepage

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,9 @@ class WelcomeController < ApplicationController
   def index
     @title = "Home"
     @body_class = " class=\"collapsing_header\"".html_safe
+
+    ids = Project.pluck(:id).shuffle[0..2]
+    @projects = Project.where(id: ids)
   end
 
   def about
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+    Project.create([
+        {
+          title: "Sweat X",
+          subtitle: "Get's the Sweat Out",
+          description: "Lorem ipsum dolor sit amet, postea propriae corrumpit has ne, causae epicuri vel at. Cu nibh vivendum nam, ex populo antiopam duo. Suscipit rationibus intellegam sed ne, justo denique vis eu, eu option incorrupte voluptatibus nam. Soluta veritus moderatius id vim. Vim agam epicurei hendrerit ne. Et electram imperdiet ullamcorper est, ne ullum sonet civibus mel. Noster iracundia disputando at mei. Eum facer inermis maiorum ex, liber sadipscing eos no, sea id magna cetero. Mei sensibus petentium consectetuer ex, an mel semper legendos. Agam soluta cu sed, et vim constituto contentiones. An eum veri utroque singulis, pro aeque laudem petentium no, cu blandit lobortis sit. Eam alii graeci evertitur no, primis quidam evertitur ex eos.Ut possit torquatos pro, usu hinc delectus disputationi ex. Eros euismod in sed, vis facer latine ne. Tantas lobortis ius ne, sed assentior deterruisset at. Nonumes efficiantur pri at.An his tibique expetenda. At sale illum salutatus nam, ius at aperiri oblique intellegat, te sint partem perfecto ius. Ius ea libris qualisque, ei oblique nostrud quo. Case eripuit disputando in sea. Mel dolorem consequuntur at, ornatus deleniti quaestio et has.Ut vocibus facilisis salutatus eos. An error appareat nec. In atqui albucius pri, quo an scaevola adipisci praesent, mutat molestie noluisse in per. Mei ei causae erroribus, sed id brute scriptorem reformidans, mel impedit pertinacia id. Probatus pertinax corrumpit pro ea.",
+          category: "Automotive"
+        },
+        {
+          title: "Presto Pro Pen",
+          subtitle: "Get's the scratches out",
+          description: "Lorem ipsum dolor sit amet, postea propriae corrumpit has ne, causae epicuri vel at. Cu nibh vivendum nam, ex populo antiopam duo. Suscipit rationibus intellegam sed ne, justo denique vis eu, eu option incorrupte voluptatibus nam. Soluta veritus moderatius id vim. Vim agam epicurei hendrerit ne. Et electram imperdiet ullamcorper est, ne ullum sonet civibus mel. Noster iracundia disputando at mei. Eum facer inermis maiorum ex, liber sadipscing eos no, sea id magna cetero. Mei sensibus petentium consectetuer ex, an mel semper legendos. Agam soluta cu sed, et vim constituto contentiones. An eum veri utroque singulis, pro aeque laudem petentium no, cu blandit lobortis sit. Eam alii graeci evertitur no, primis quidam evertitur ex eos.Ut possit torquatos pro, usu hinc delectus disputationi ex. Eros euismod in sed, vis facer latine ne. Tantas lobortis ius ne, sed assentior deterruisset at. Nonumes efficiantur pri at.An his tibique expetenda. At sale illum salutatus nam, ius at aperiri oblique intellegat, te sint partem perfecto ius. Ius ea libris qualisque, ei oblique nostrud quo. Case eripuit disputando in sea. Mel dolorem consequuntur at, ornatus deleniti quaestio et has.Ut vocibus facilisis salutatus eos. An error appareat nec. In atqui albucius pri, quo an scaevola adipisci praesent, mutat molestie noluisse in per. Mei ei causae erroribus, sed id brute scriptorem reformidans, mel impedit pertinacia id. Probatus pertinax corrumpit pro ea.",
+          category: "Automotive"
+        },
+        {
+          title: "Water Tectonics",
+          subtitle: "The latest breakthrough in water filtration",
+          description: "Lorem ipsum dolor sit amet, postea propriae corrumpit has ne, causae epicuri vel at. Cu nibh vivendum nam, ex populo antiopam duo. Suscipit rationibus intellegam sed ne, justo denique vis eu, eu option incorrupte voluptatibus nam. Soluta veritus moderatius id vim. Vim agam epicurei hendrerit ne. Et electram imperdiet ullamcorper est, ne ullum sonet civibus mel. Noster iracundia disputando at mei. Eum facer inermis maiorum ex, liber sadipscing eos no, sea id magna cetero. Mei sensibus petentium consectetuer ex, an mel semper legendos. Agam soluta cu sed, et vim constituto contentiones. An eum veri utroque singulis, pro aeque laudem petentium no, cu blandit lobortis sit. Eam alii graeci evertitur no, primis quidam evertitur ex eos.Ut possit torquatos pro, usu hinc delectus disputationi ex. Eros euismod in sed, vis facer latine ne. Tantas lobortis ius ne, sed assentior deterruisset at. Nonumes efficiantur pri at.An his tibique expetenda. At sale illum salutatus nam, ius at aperiri oblique intellegat, te sint partem perfecto ius. Ius ea libris qualisque, ei oblique nostrud quo. Case eripuit disputando in sea. Mel dolorem consequuntur at, ornatus deleniti quaestio et has.Ut vocibus facilisis salutatus eos. An error appareat nec. In atqui albucius pri, quo an scaevola adipisci praesent, mutat molestie noluisse in per. Mei ei causae erroribus, sed id brute scriptorem reformidans, mel impedit pertinacia id. Probatus pertinax corrumpit pro ea.",
+          category: "Energy"
+        },
+        {
+          title: "Gas Off",
+          subtitle: "Get's the Gas Out",
+          description: "Lorem ipsum dolor sit amet, postea propriae corrumpit has ne, causae epicuri vel at. Cu nibh vivendum nam, ex populo antiopam duo. Suscipit rationibus intellegam sed ne, justo denique vis eu, eu option incorrupte voluptatibus nam. Soluta veritus moderatius id vim. Vim agam epicurei hendrerit ne. Et electram imperdiet ullamcorper est, ne ullum sonet civibus mel. Noster iracundia disputando at mei. Eum facer inermis maiorum ex, liber sadipscing eos no, sea id magna cetero. Mei sensibus petentium consectetuer ex, an mel semper legendos. Agam soluta cu sed, et vim constituto contentiones. An eum veri utroque singulis, pro aeque laudem petentium no, cu blandit lobortis sit. Eam alii graeci evertitur no, primis quidam evertitur ex eos.Ut possit torquatos pro, usu hinc delectus disputationi ex. Eros euismod in sed, vis facer latine ne. Tantas lobortis ius ne, sed assentior deterruisset at. Nonumes efficiantur pri at.An his tibique expetenda. At sale illum salutatus nam, ius at aperiri oblique intellegat, te sint partem perfecto ius. Ius ea libris qualisque, ei oblique nostrud quo. Case eripuit disputando in sea. Mel dolorem consequuntur at, ornatus deleniti quaestio et has.Ut vocibus facilisis salutatus eos. An error appareat nec. In atqui albucius pri, quo an scaevola adipisci praesent, mutat molestie noluisse in per. Mei ei causae erroribus, sed id brute scriptorem reformidans, mel impedit pertinacia id. Probatus pertinax corrumpit pro ea.",
+          category: "Automotive"
+        },
+        {
+          title: "True Flavorware",
+          subtitle: "Get's the taste Out",
+          description: "Lorem ipsum dolor sit amet, postea propriae corrumpit has ne, causae epicuri vel at. Cu nibh vivendum nam, ex populo antiopam duo. Suscipit rationibus intellegam sed ne, justo denique vis eu, eu option incorrupte voluptatibus nam. Soluta veritus moderatius id vim. Vim agam epicurei hendrerit ne. Et electram imperdiet ullamcorper est, ne ullum sonet civibus mel. Noster iracundia disputando at mei. Eum facer inermis maiorum ex, liber sadipscing eos no, sea id magna cetero. Mei sensibus petentium consectetuer ex, an mel semper legendos. Agam soluta cu sed, et vim constituto contentiones. An eum veri utroque singulis, pro aeque laudem petentium no, cu blandit lobortis sit. Eam alii graeci evertitur no, primis quidam evertitur ex eos.Ut possit torquatos pro, usu hinc delectus disputationi ex. Eros euismod in sed, vis facer latine ne. Tantas lobortis ius ne, sed assentior deterruisset at. Nonumes efficiantur pri at.An his tibique expetenda. At sale illum salutatus nam, ius at aperiri oblique intellegat, te sint partem perfecto ius. Ius ea libris qualisque, ei oblique nostrud quo. Case eripuit disputando in sea. Mel dolorem consequuntur at, ornatus deleniti quaestio et has.Ut vocibus facilisis salutatus eos. An error appareat nec. In atqui albucius pri, quo an scaevola adipisci praesent, mutat molestie noluisse in per. Mei ei causae erroribus, sed id brute scriptorem reformidans, mel impedit pertinacia id. Probatus pertinax corrumpit pro ea.",
+          category: "Medical"
+        },
+    ])
+
+    Category.create([
+        {name: "Automotive"},
+        {name: "Energy"},
+        {name: "Medical"},
+        {name: "Industrial"}
+    ])
