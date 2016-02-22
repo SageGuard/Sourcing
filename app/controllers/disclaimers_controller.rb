d42f6bf0ed8ca5022e3741f831f5d614c2c93a4a
Anthony Mortimer <amortimer@sageguard.com>
2016-02-22 13:05:25 -0500

    Removed registerable for user, added disclaimer views

---
+++ app/controllers/disclaimers_controller.rb
@@ -0,0 +1,10 @@
+class DisclaimersController < ApplicationController
+  def terms
+  end
+
+  def refund
+  end
+
+  def privacy
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -77,6 +77,7 @@
     ])
 
     puts "Creating Admin..."
+    puts "Defaulting to 'test@test.com', 'password'"
     User.create! email: 'test@test.com',
                   password: 'password',
                   password_confirmation: 'password',
